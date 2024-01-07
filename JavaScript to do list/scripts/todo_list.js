5d03422d09a5c46f126ab388efe939b1cfddb24c
Çağrı <karpuzcagri@gmail.com>
2024-01-07 16:06:26 +0300

    Main

--- JavaScript to do list/scripts/todo_list.js
+++ JavaScript to do list/scripts/todo_list.js
@@ -1,13 +1,30 @@
-const todoList = [];
+const todoList = [''];
+//const todoList = ["Make dinner", "Wash dishes"];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHtml = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p  style="margin-bottom: 2px;  margin-top: 10px;">${todo}</p>`;
+    todoListHtml += html;
+  }
+
+  //console.log(todoListHtml);
+  document.querySelector('.js-todo-list').innerHTML = todoListHtml;
+};
 
 function addtodo() {
-    const inputElement = document.querySelector('.js-name-input');
-    const name = inputElement.value;
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
 
-    //console.log(name);
+  //console.log(name);
 
-    todoList.push(name);
-    console.log(todoList);
+  todoList.push(name);
+  console.log(todoList);
 
-    inputElement.value = '';
+  inputElement.value = '';
+  renderTodoList();
 }
